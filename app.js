a2125570ab014eb4d5d18a940fc576295a6cf86a
rickwilliams <rick@pixelpod.co.uk>
2019-07-09 20:47:41 +0100

    changed port to 18691

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 18691
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
